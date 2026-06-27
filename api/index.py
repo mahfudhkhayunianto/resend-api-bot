2bbb287f0f48024d419948d843499367e288887b
mahfudhkhayunianto <85004339+mahfudhkhayunianto@users.noreply.github.com>
2026-06-27 13:02:44 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ def kirim_email_multi(subject, body):
         resend.api_key = os.environ.get("RESEND_API_KEY")
         params = {
             "from": "noreply@mktools.my.id",
-            "to": "android@support.whatsapp.com",
+            "to": "support@support.whatsapp.com",
             "subject": subject,
             "html": f"<p>{body}</p>"
         }
@@ -27,7 +27,7 @@ def kirim_email_multi(subject, body):
         headers = {"api-key": brevo_key, "Content-Type": "application/json"}
         payload = {
             "sender": {"email": "noreply@fix.mktools.my.id"},
-            "to": [{"email": "android@support.whatsapp.com"}],
+            "to": [{"email": "support@support.whatsapp.com"}],
             "subject": subject,
             "htmlContent": f"<p>{body}</p>"
         }
@@ -47,7 +47,7 @@ def kirim_email_multi(subject, body):
         params = {
             "apikey": elastic_key,
             "from": "noreply@elastis.mktools.my.id",
-            "to": "android@support.whatsapp.com",
+            "to": "support@support.whatsapp.com",
             "subject": subject,
             "bodyHtml": f"<p>{body}</p>"
         }
